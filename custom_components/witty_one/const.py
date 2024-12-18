8e5083ba16908b82dddcc8df68a15decb42ebe26
Nicolas Graziano <nicolas.graziano@gmail.com>
2024-12-18 18:19:38 +0000

    Rename template files

--- custom_components/witty_one/const.py
+++ custom_components/witty_one/const.py
@@ -1,8 +1,8 @@
-"""Constants for integration_blueprint."""
+"""Constants for witty_one."""
 
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-DOMAIN = "integration_blueprint"
+DOMAIN = "witty_one"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
--- custom_components/witty_one/coordinator.py
+++ custom_components/witty_one/coordinator.py
@@ -1,4 +1,4 @@
-"""DataUpdateCoordinator for integration_blueprint."""
+"""DataUpdateCoordinator for witty_one."""
 
 from __future__ import annotations
 
