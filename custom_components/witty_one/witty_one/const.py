6c944cd13f328a948e98aecaf8c1fdcd753c5fcd
Nicolas Graziano <nicolas.graziano+github@gmail.com>
2025-08-31 20:37:01 +0200

    Add some UUID

--- custom_components/witty_one/witty_one/const.py
+++ custom_components/witty_one/witty_one/const.py
@@ -29,15 +29,31 @@ HMI_BOARD_VERSION_UUID = _state_uuid("0302")
 STARTUP_COUNT_UUID = _state_uuid("0410")  # length 4+0x0004 format : <HQ
 DURATIONS_UUID = _state_uuid("0510")  # length 4+0x0010 format : <HIIII
 COMMUTATION_UUID = _state_uuid("0610")  # length 4+0x0020 format : <HQQQQ
+CAR_DETECT_UUID = _state_uuid("0710")  # length 4+0x0004 format : <HQ
+CHARGING_NUMBER_UUID = _state_uuid("0810")  # length 4+0x0004 format : <HQ
 
+CABLE_LOCK_UUID = _state_uuid("0c10")  # length 4+0x0002 format : <HH
 
 # Electric
 ENERGY_UUID = _state_uuid("4010")
 ELECTRIC_STATE_UUID = _state_uuid("4110")
+NEUTRAL_VOLTAGE = _state_uuid("4210")
 
 # Temp
 AMBIENT_TEMP_UUID = _state_uuid("5010")
 RELAY_TEMP_UUID = _state_uuid("5110")
+UNK_TEMP_UUID = _state_uuid("5210")
 
+# length 4+0x000C format : <HHHHHHH 0500 1400 1000 0000 0000 0300
+CONNECTION_STATE_UUID = _state_uuid("6010")
 
 SESSION_STATE_UUID = _state_uuid("6110")
+
+DATE_STR_UUID = _state_uuid("0120")
+
+# Config
+CONFIG_CABLE_LOCK_UUID = _state_uuid("0880")  # length 4+0x0001
+CONFIG_BADGE_UUID = _state_uuid("2180")
+
+#  MAC (6 bytes), NAME 32 bytes, STATE 1 byte
+CONFIG_ADMIN_PHONES_UUID = _state_uuid("3080")
